e6914d3aad811944b59d10c0acd2d43c6b13aaaf
physicshinzui <physicshinzui@gmail.com>
2023-06-13 22:09:57 +0900

    update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,5 @@
 #!/bin/bash 
 
-ls *.gro *.tpr *# *.edr *.trr *.xtc *.top *.itp *.mdp *.log *.cpt
+ls tleap.in system* *.gro *.tpr *# *.edr *.trr *.xtc *.top *.itp *.mdp *.log *.cpt
 read -p "Are you really sure?[Enter]"
-read -p "Are you really sure?[Enter]"
-mkdir tmp ; cp -rp *.gro *.tpr *# *.edr *.trr *.xtc *.top *.itp *.mdp *.log *.cpt tmp
-
-rm -v *.gro *.tpr *# *.edr *.trr *.xtc *.top *.itp *.mdp *.log *.cpt
+rm -v tleap.in system* *.gro *.tpr *# *.edr *.trr *.xtc *.top *.itp *.mdp *.log *.cpt
