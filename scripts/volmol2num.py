4acdc4c5f26435076e694d6d1d1ca6ca7841fee1
physicshinzui <physicshinzui@gmail.com>
2023-06-14 18:35:46 +0900

    modify

--- scripts/volmol2num.py
+++ scripts/volmol2num.py
@@ -2,6 +2,14 @@ import subprocess
 import re
 import sys
 import os
+"""
+    Args:
+        input gro
+        concentration
+
+    Return:
+        number of noble gas atoms
+"""
 
 input_gro = sys.argv[1]
 concentration_molar = float(sys.argv[2])
@@ -11,7 +19,7 @@ output = subprocess.check_output(f"gmx editconf -f {input_gro} -o tmp.gro", shel
 
 # Extract volume from output
 volume = float(re.search(r"Volume: (\d+\.\d+)", output.decode()).group(1))
-print("Volume nm^3 = ", volume)
+#print("Volume nm^3 = ", volume)
 
 # Calculate number of atoms for 0.1 M concentration (in nm^-3)
 avogadro_number = 6.02214076e23 # per mol
@@ -19,5 +27,6 @@ volume_litres = volume * 1e-24  # convert nm^3 to L
 
 number_of_atoms = int(concentration_molar * avogadro_number * volume_litres)
 
-print("#atoms to be inserted: ", number_of_atoms)
+#print("#atoms to be inserted: ", number_of_atoms)
+print(number_of_atoms)
 os.remove("tmp.gro")
