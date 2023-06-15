0ca3ab5f517a71638f2b66eb3966d8fe211e43a8
physicshinzui <physicshinzui@gmail.com>
2023-06-15 21:12:33 +0900

    update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
-ls tleap.in system* *.gro *.tpr *# *.edr *.trr *.xtc *.top *.itp *.mdp *.log *.cpt
+ls p4a* tleap.in system* *.gro *.tpr *# *.edr *.trr *.xtc *.top *.itp *.mdp *.log *.cpt
 read -p "Are you really sure?[Enter]"
-rm -v tleap.in system* *.gro *.tpr *# *.edr *.trr *.xtc *.top *.itp *.mdp *.log *.cpt
+rm -v p4a* tleap.in system* *.gro *.tpr *# *.edr *.trr *.xtc *.top *.itp *.mdp *.log *.cpt
