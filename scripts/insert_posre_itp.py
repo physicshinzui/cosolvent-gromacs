c566bd63ffc90f55e5e8685beb7629dcbf92a9a0
physicshinzui <physicshinzui@gmail.com>
2023-06-17 22:58:08 +0900

    Modify insert_posres script

--- scripts/insert_posre_itp.py
+++ scripts/insert_posre_itp.py
@@ -1,11 +1,17 @@
 
 import sys
-input_file = sys.argv[1]
-output_file = sys.argv[2]
+import pdbfixer 
+import shutil
+
+def count_nchains(pdbfilename):
+    return len(list(pdbfixer.PDBFixer(filename=pdbfilename).topology.chains()))
+    
+top = sys.argv[1]
+pdb = sys.argv[2]
 
 # Insert ifdef lines before the 2nd moleculetype line.
-moleculetype_target_counter = 2
-with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+moleculetype_target_counter = count_nchains(pdb) + 1
+with open(top, 'r') as infile, open('tmp', 'w') as outfile:
     count = 0
     for line in infile:
         if '[ moleculetype' in line:
@@ -13,3 +19,4 @@ with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
             if count == moleculetype_target_counter:
                 outfile.write('#ifdef POSRES\n#include "posre.itp"\n#endif\n\n')
         outfile.write(line)
+shutil.move('tmp', top)
